ef8df9e0ada997f2b34a8f69c40cf3a8f5a0f9e2
KhoaDauTay <54214642+KhoaDauTay@users.noreply.github.com>
2020-05-18 18:33:09 +0700

    Clean debug set up project

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\uit floder\onedrive - trường đh cntt - university of information technology\teamcrushcaring\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
